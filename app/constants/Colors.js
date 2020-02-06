cd5af2cbe833d7297ff3061b218e5cce4a403378
Fanny Huang <>
2020-02-06 19:20:43 +0100

    Add app's colors

---
+++ app/constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#0042da';
+const gray = '#9399ac';
+const black = '#020202';
+const white = '#ffffff';
+const backgroundColor = '#eff2f9';
+
+export default {
+    tintColor,
+    backgroundColor,
+    black,
+    white,
+    activeTabColor: tintColor,
+    inactiveTabColor: gray,
+}
