f5fe73b5ed19dea04589b78dc96465ac38e3e5e8
Fanny Huang <>
2020-02-06 20:06:16 +0100

    Structure InventoryView + add InventoryItemCategoryEnum

--- app/views/InventoryView.js
+++ app/views/InventoryView.js
@@ -1,20 +1,73 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet } from 'react-native';
+import InventoryItemCategoryEnum from "../../model/InventoryItemCategoryEnum";
 
 
 const inventoryList = [
-
+  {
+    name: "Cartier Ring",
+    category: InventoryItemCategoryEnum.JEWELLERY,
+    dateBought: Date(),
+    price: 5780,
+    description: "",
+    photo: null,
+    invoice: null,
+  },
+  {
+    name: "Lou.Yetu Necklace",
+    category: InventoryItemCategoryEnum.JEWELLERY,
+    dateBought: Date(),
+    price: 60,
+    description: "",
+    photo: null,
+    invoice: null,
+  },
+  {
+    name: "Chanel Pearl Bracelet",
+    category: InventoryItemCategoryEnum.JEWELLERY,
+    dateBought: Date(),
+    price: 2100,
+    description: "",
+    photo: null,
+    invoice: null,
+  },
+  {
+    name: "Messika Earrings",
+    category: InventoryItemCategoryEnum.JEWELLERY,
+    dateBought: Date(),
+    price: 10090,
+    description: "",
+    photo: null,
+    invoice: null,
+  },
+  {
+    name: "Chopard Watch",
+    category: InventoryItemCategoryEnum.JEWELLERY,
+    dateBought: Date(),
+    price: 13420,
+    description: "",
+    photo: null,
+    invoice: null,
+  },
 ];
 
+getInventoryItems = (inventoryList) => {
+  let items = [];
+  inventoryList.forEach(item => {
 
+  });
+  return items;
+};
 
 const view = (
   <ScrollView style={styles.container}>
+    {getInventoryItems()}
   </ScrollView>
 );
 
 const styles = StyleSheet.create({
   container: {
+    flex: '1',
     width: '100%',
     height: '100%',
     backgroundColor: 'red',
---
+++ model/InventoryItemCategoryEnum.js
@@ -0,0 +1,6 @@
+export default Object.freeze({
+  ARTS: 'Arts',
+  ELECTRONICS: 'Electronics',
+  JEWELLERY: 'Jewellery',
+  MUSIC_INSTRUMENT: 'Music Instrument'
+});
