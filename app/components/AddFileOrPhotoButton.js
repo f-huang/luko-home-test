9bdefa4d09b3064ab0abbc41ec255b3085f8743b
Fanny Huang <>
2020-02-07 20:34:22 +0100

    Add Component to add photo/file and display them + integrade this componenent in inventory view

---
+++ app/components/AddFileOrPhotoButton.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import PropTypes from 'prop-types';
+import Colors from "../constants/Colors";
+import {isStringNullOrEmpty} from "../tools/isStringNullOrEmpty";
+
+class AddFileOrPhotoButton extends React.Component {
+  static propTypes = {
+    alt: PropTypes.string,
+    imageUri: PropTypes.string,
+    buttonPurpose: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    imageUri: null,
+    alt: null,
+    buttonPurpose: AddFileOrPhotoButton.ButtonPurpose.ADD_PHOTO,
+  };
+
+  static get ButtonPurpose() {
+    return {
+      ADD_FILE: 'Add file',
+      ADD_PHOTO: 'Add photo'
+    }
+  };
+
+  render() {
+    const textLabel = this.props.buttonPurpose;
+    return (
+      <View style={styles.container}>
+        <View style={styles.innerContainer}>
+          <TouchableOpacity style={styles.button} onPress={this.props.onChange} alt={this.props.alt}>
+            <Image style={styles.image} source={{ uri: this.props.imageUri }}/>
+            { isStringNullOrEmpty(this.props.imageUri) &&  <Text style={styles.text}>{textLabel}</Text> }
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    position: 'relative',
+    display: 'flex',
+    width: 120,
+    height: 120,
+    borderWidth: 1.5,
+    borderRadius: 5,
+    borderColor: Colors.gray,
+    borderStyle: 'dashed',
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    backgroundColor: Colors.white,
+    zIndex: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    position: 'absolute',
+    bottom: 12
+  }
+
+});
+
+export default AddFileOrPhotoButton;
--- app/screens/CreateInventoryItemScreen.js
+++ app/screens/CreateInventoryItemScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {ScrollView, View, Text, StyleSheet, Button} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import Colors from "../constants/Colors";
 import CreateInventoryItemForm from "../views/CreateInventoryItemForm";
 import {isStringNullOrEmpty} from "../tools/isStringNullOrEmpty";
+import AddFileOrPhotoButton from "../components/AddFileOrPhotoButton";
 
+
+const imagePickerOptions = {
+  title: 'Select Image',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
 
 
 class CreateInventoryItemScreen extends React.Component {
@@ -42,6 +52,8 @@ class CreateInventoryItemScreen extends React.Component {
   componentDidMount() {
     this.props.navigation.setParams({
       handleSubmit: this.handleSubmit,
+      isFormValid: this.isFormValid,
+      handlePhotoPicker: this.handlePhotoPicker,
     });
   }
 
@@ -51,6 +63,21 @@ class CreateInventoryItemScreen extends React.Component {
     if (this.isFormValid()) {
       // Add item into DB.
     }
+  };
+
+  handlePhotoPicker = () => {
+    ImagePicker.showImagePicker(imagePickerOptions, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        this.setState({ value: { photo: response }})
+      }
+    });
   };
 
   isFormValid = () => {
@@ -64,11 +91,15 @@ class CreateInventoryItemScreen extends React.Component {
   };
 
   render() {
+    const imageUri = this.state.value.photo !== null ? this.state.value.photo.uri : "";
+
     return (
       <ScrollView contentContainerStyle={styles.container}>
+        <AddFileOrPhotoButton imageUri={imageUri} onChange={this.handlePhotoPicker}/>
         <CreateInventoryItemForm
           ref={c => this.childRef = c}
           value={this.state.value}/>
+
       </ScrollView>
 
     );
